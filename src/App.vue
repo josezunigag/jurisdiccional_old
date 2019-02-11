25f68a247635ef0515c482dd5de70941004ef195
Sebastian <sebastiansilvagomez@gmail.com>
2019-02-11 14:55:29 -0300

    Modificacion Funcional

--- src/App.vue
+++ src/App.vue
@@ -21,10 +21,10 @@
                       <a href="javascript:void(0)" class="sidebartoggler font-20 waves-effect waves-light"><i class="icon-arrow-left-circle"></i></a>
                   </li>
                   <li>
-                      <form role="search" class="app-search hidden-xs">
+                      <!-- <form role="search" class="app-search hidden-xs">
                           <i class="icon-magnifier"></i>
                           <input type="text" placeholder="Search..." class="form-control">
-                      </form>
+                      </form> -->
                   </li>
               </ul>
               <ul class="nav navbar-top-links navbar-right pull-right">
--- src/main.js
+++ src/main.js
@@ -2,18 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import $ from 'jquery'
-import LoadScript from 'vue-plugin-load-script';
+// import $ from 'jquery'
+// import LoadScript from 'vue-plugin-load-script';
 import 'animate/index.js'
 import 'waves/dist/waves.js'
-import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap/dist/css/bootstrap.css'
 
-window.$ = $
-window.jQuery = $
-Vue.use(LoadScript);
-Vue.loadScript('/js/jquery.slimscroll.min.js')
-Vue.loadScript("/js/custom.js")
-Vue.loadScript("/js/sidebarmenu.js")
+// window.$ = $
+// window.jQuery = $
+// Vue.use(LoadScript);
+// Vue.loadScript('/js/jquery.slimscroll.min.js')
+// Vue.loadScript("/js/custom.js")
+// Vue.loadScript("/js/sidebarmenu.js")
 
 
 
