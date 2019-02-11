d0f146b6677efee31a8fe2410bac0ca333e5e80c
Sebastian <sebastiansilvagomez@gmail.com>
2019-02-11 14:13:55 -0300

    Bootstrap y animate

--- src/main.js
+++ src/main.js
@@ -2,8 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'bootstrap/dist/css/bootstrap.css'
+import $ from 'jquery'
+import LoadScript from 'vue-plugin-load-script';
 import 'animate/index.js'
+import 'waves/dist/waves.js'
+import 'bootstrap/dist/css/bootstrap.css'
+
+window.$ = $
+window.jQuery = $
+Vue.use(LoadScript);
+Vue.loadScript('/js/jquery.slimscroll.min.js')
+Vue.loadScript("/js/custom.js")
+Vue.loadScript("/js/sidebarmenu.js")
+
+
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,12 @@
 <template>
   <div class="home">
-    <!-- <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet"> -->
-    <!-- ===== Plugin CSS ===== -->
-    <!-- ===== Animation CSS ===== -->
-    <link href="/css/animate.css" rel="stylesheet">
-    <!-- ===== Custom CSS ===== -->
-    <link href="/css/style.css" rel="stylesheet">
-    <!-- ===== Color CSS ===== -->
-    <link href="/css/colors/default.css" id="theme" rel="stylesheet">     
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import ComponentsHome from '@/components/ComponentsHome.vue'
-
+    
 export default {
   name: 'home',
   components: {
