faf4f549b727869ebaef74efa7ee74f6f7accf28
Sebastian Silva <sebastiansilvagomez@gmail.com>
2019-02-13 15:46:46 -0300

    Agregacion de Schema

--- src/connect.js
+++ src/connect.js
@@ -5,16 +5,10 @@
 const mongoose = require('mongoose');
  
 // Le indicamos a Mongoose que haremos la conexión con Promesas
-mongoose.Promise = global.Promise;
+//mongoose.Promise = global.Promise;
  
 // Usamos el método connect para conectarnos a nuestra base de datos
-const dbase = mongoose.connect('mongodb://10.13.142.224/juridiccional', {useMongoClient: true})
-        .then(() => {
- 
-                // Cuando se realiza la conexión, lanzamos este mensaje por consola
-            console.log('La conexión a MongoDB se ha realizado correctamente!!');
-        })
-        .catch(err => console.log(err));
-        // Si no se conecta correctamente escupimos el error
+mongoose.connect('mongodb://10.13.142.224/juridiccional');
+// Si no se conecta correctamente escupimos el error
 
-module.exports = dbase
+module.exports = mongoose
--- src/models/IngresoModel.js
+++ src/models/IngresoModel.js
@@ -1,8 +1,25 @@
 const dbase      = require('../connect'); // Requiere Coneccion.js 
 const { Schema } = require('mongoose');
 
-const ingresos = dbase.model('ingresos', new Schema({ // Definicion Implicito de Schema
-    cod_tribunal: Number
-}));
+    var ingresos = dbase.model('ingresos', new Schema({ // Definicion Implicito de Schema
+        competencia_id: Number,
+        cod_corte: Number,
+        cod_tribunal: Number,
+        ano: Number,
+        mes: Number,
+        fec_ingreso: Date,
+        rit: String,
+        tipo_causa_id: Number,
+        tip_causa: String,
+        gls_tipo_causa: String,
+        tipo_ing_causa_id: Number,
+        gls_ing_causa: String,
+        materia_id: Number,
+        glosa_materia: String
+    }));
 
-module.exports = ingresos // Exportamos los atributos de ingresos.
+    ingresos.findOne().then(( result) => {
+        console.log(result)
+    })
+    
+    module.exports = ingresos
