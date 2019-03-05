15a08d70c247a9895b7bde8705e4457e3ef766f2
Sebastian Silva <sebastiansilvagomez@gmail.com>
2019-03-04 21:44:48 -0300

    Modificacion Paso a produccion

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
-const url = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'http://vmquantumsuprema.cadm.pjud:3455'
+const url = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'http://vmetlbi.cadm.pjud:8080'
 export {
   url
 }
