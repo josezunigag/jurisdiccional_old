6d6ca1226455668bf9369f592e5b43d0ceda58d7
Sebastian Silva <sebastiansilvagomez@gmail.com>
2019-03-06 07:39:59 -0300

    Correcion de Puerto

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
-const url = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'http://vmetlbi.cadm.pjud:8080'
+const url = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'http://vmetlbi.cadm.pjud'
 export {
   url
 }
