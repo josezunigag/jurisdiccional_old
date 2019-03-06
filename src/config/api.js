bf5145690e56e9cfe92a7ff18fe124e5e2010a20
Sebastian Silva <sebastiansilvagomez@gmail.com>
2019-03-06 07:48:56 -0300

    .

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
-const url = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'http://vmetlbi.cadm.pjud'
+const url = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'http://vmetlbi.cadm.pjud:8080'
 export {
   url
 }
