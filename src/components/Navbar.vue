2ba693e2433f1885c4760926bc1356ecb170a46e
Sebastian <sebastiansilvagomez@gmail.com>
2019-03-08 00:29:47 -0300

    Correccion

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -19,158 +19,11 @@
 				<li>
 					<a href="javascript:void(0)" class="sidebartoggler font-20 waves-effect waves-light" @click="resize"><i class="icon-arrow-left-circle"></i></a>
 				</li>
-				<li>
-					<!-- <form role="search" class="app-search hidden-xs">
-						<i class="icon-magnifier"></i>
-						<input type="text" placeholder="Search..." class="form-control">
-					</form> -->
+				<li class="nav navbar-top-links navbar-center">
+					<h2 id="tribunal">{{gls_tribunal}}</h2>	
 				</li>
 			</ul>
-			<ul class="nav navbar-top-links navbar-right pull-right">
-			<!-- <ul class="nav navbar-top-links navbar-right pull-right">
-				<li class="dropdown">
-					<a class="dropdown-toggle waves-effect waves-light font-20" data-toggle="dropdown" href="javascript:void(0);">
-						<i class="icon-speech"></i>
-						<span class="badge badge-xs badge-danger">6</span>
-					</a> -->
-					<!-- <ul class="dropdown-menu mailbox animated bounceInDown">
-						<li>
-							<div class="drop-title">You have 4 new messages</div>
-						</li>
-						<li>
-							<div class="message-center">
-								<a href="javascript:void(0);">
-									<div class="user-img">
-										<img src="../plugins/images/users/1.jpg" alt="user" class="img-circle">
-										<span class="profile-status online pull-right"></span>
-									</div>
-									<div class="mail-contnet">
-										<h5>Pavan kumar</h5>
-										<span class="mail-desc">Just see the my admin!</span>
-										<span class="time">9:30 AM</span>
-									</div>
-								</a>
-								<a href="javascript:void(0);">
-									<div class="user-img">
-										<img src="../plugins/images/users/2.jpg" alt="user" class="img-circle">
-										<span class="profile-status busy pull-right"></span>
-									</div>
-									<div class="mail-contnet">
-										<h5>Sonu Nigam</h5>
-										<span class="mail-desc">I've sung a song! See you at</span>
-										<span class="time">9:10 AM</span>
-									</div>
-								</a>
-								<a href="javascript:void(0);">
-									<div class="user-img">
-										<img src="../plugins/images/users/3.jpg" alt="user" class="img-circle"><span class="profile-status away pull-right"></span>
-									</div>
-									<div class="mail-contnet">
-										<h5>Arijit Sinh</h5>
-										<span class="mail-desc">I am a singer!</span>
-										<span class="time">9:08 AM</span>
-									</div>
-								</a>
-								<a href="javascript:void(0);">
-									<div class="user-img">
-										<img src="../plugins/images/users/4.jpg" alt="user" class="img-circle">
-										<span class="profile-status offline pull-right"></span>
-									</div>
-									<div class="mail-contnet">
-										<h5>Pavan kumar</h5>
-										<span class="mail-desc">Just see the my admin!</span>
-										<span class="time">9:02 AM</span>
-									</div>
-								</a>
-							</div>
-						</li>
-						<li>
-							<a class="text-center" href="javascript:void(0);">
-								<strong>See all notifications</strong>
-								<i class="fa fa-angle-right"></i>
-							</a>
-						</li>
-					</ul> -->
-				<!-- </li>
-				<li class="dropdown">
-					<a class="dropdown-toggle waves-effect waves-light font-20" data-toggle="dropdown" href="javascript:void(0);">
-						<i class="icon-calender"></i>
-						<span class="badge badge-xs badge-danger">3</span>
-					</a>
-					<ul class="dropdown-menu dropdown-tasks animated slideInUp">
-						<li>
-							<a href="javascript:void(0);">
-								<div>
-									<p>
-										<strong>Task 1</strong>
-										<span class="pull-right text-muted">40% Complete</span>
-									</p>
-									<div class="progress progress-striped active">
-										<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
-											<span class="sr-only">40% Complete (success)</span>
-										</div>
-									</div>
-								</div>
-							</a>
-						</li>
-						<li class="divider"></li>
-						<li>
-							<a href="javascript:void(0);">
-								<div>
-									<p>
-										<strong>Task 2</strong>
-										<span class="pull-right text-muted">20% Complete</span>
-									</p>
-									<div class="progress progress-striped active">
-										<div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
-											<span class="sr-only">20% Complete</span>
-										</div>
-									</div>
-								</div>
-							</a>
-						</li>
-						<li class="divider"></li>
-						<li>
-							<a href="javascript:void(0);">
-								<div>
-									<p>
-										<strong>Task 3</strong>
-										<span class="pull-right text-muted">60% Complete</span>
-									</p>
-									<div class="progress progress-striped active">
-										<div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
-											<span class="sr-only">60% Complete (warning)</span>
-										</div>
-									</div>
-								</div>
-							</a>
-						</li>
-						<li class="divider"></li>
-						<li>
-							<a href="javascript:void(0);">
-								<div>
-									<p>
-										<strong>Task 4</strong>
-										<span class="pull-right text-muted">80% Complete</span>
-									</p>
-									<div class="progress progress-striped active">
-										<div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
-											<span class="sr-only">80% Complete (danger)</span>
-										</div>
-									</div>
-								</div>
-							</a>
-						</li>
-						<li class="divider"></li>
-						<li>
-							<a class="text-center" href="javascript:void(0);">
-								<strong>See All Tasks</strong>
-								<i class="fa fa-angle-right"></i>
-							</a>
-						</li>
-					</ul>
-				</li>
-				-->
+			<ul class="nav navbar-top-links navbar-right pull-right">			
 				<li class="right-side-toggle">
 					<a class="right-side-toggler waves-effect waves-light b-r-0 font-20" href="javascript:void(0)">
 						<i class="icon-settings"></i>
@@ -183,11 +36,15 @@
 </template>
 
 <script>
- 
+import {url} from '@/config/api'
+import store from 'store'
 export default {
 	name: 'Navbar',
-	mounted(){
-		
+	data(){
+		return{
+			gls_tribunal : '',
+			local: store.get('user')
+		}
 	},
 	methods: {
 		resize(){
@@ -230,7 +87,40 @@ export default {
         if (height < 1) height = 1;
         if (height > topOffset) {
             $(".page-wrapper").css("min-height", (height) + "px");
-        }
+		}
+        const axios = require("axios");
+        
+        let  url_ing = url+"/tribunales_glosa";
+        this.competencia_id = this.local.competencia_id;
+        this.cod_corte      = this.local.cod_corte;
+		this.cod_tribunal   = this.local.cod_tribunal;
+				
+		const getData = async url_ing => {
+			try {
+
+				const response = await axios.get(url_ing,{
+					params: {
+						competencia_id: this.competencia_id,
+						cod_corte: this.cod_corte, 
+						cod_tribunal: this.cod_tribunal
+					}  
+				});
+
+				const data  = response.data;
+
+				Object.values(data.data.tribunal).map((type) => {
+
+						this.gls_tribunal =  type.gls_tribunal;
+
+				})
+
+
+			} catch (error) {
+				console.log(error);
+			}
+		};
+
+		getData(url_ing);			
 	}
 	
 }
@@ -238,4 +128,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+#tribunal{
+	color: #e4ebef
+}
 </style>
